cfa486ae2841e43445416ef6c719f80bfdf7e88b
NiharKancherla <niharkancherla@gmail.com>
2019-11-30 21:23:51 +0530

    Port mapping

---
+++ src/test/java/com/chrisnihar/msscbeerinventoryservice/MsscBeerInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisnihar.msscbeerinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
